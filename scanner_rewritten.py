422bbc3098b84811245ab4825589c663ab53ecdb
Milos <milosvuksanovic53@gmail.com>
2020-05-14 21:46:42 +1000

    first function & function call - get_account()

--- scanner_rewritten.py
+++ scanner_rewritten.py
@@ -3,3 +3,18 @@ import os
 import sys
 import datetime
 import shutil
+
+#function to get user account & time accessed
+def get_account():
+    #get users account username
+    user_acc = os.getlogin()
+    #get time accessed
+    time_acc = datetime.datetime.now()
+    #print username logged in
+    print(f"User: {user_acc}")
+    #print time accessed - now
+    print(f"Last accessed:{time_acc}")
+
+
+#call get_account function
+get_account()    
