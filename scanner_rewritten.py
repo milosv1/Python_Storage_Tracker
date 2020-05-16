e9fdae00af2ec5547cab3d803a004874791c343e
Milos <milosvuksanovic53@gmail.com>
2020-05-16 18:11:34 +1000

    get_usedSpace function, called get_usedSpace

--- scanner_rewritten.py
+++ scanner_rewritten.py
@@ -13,6 +13,9 @@ user_acc = os.getlogin()
 #path needed - global - '/' root directory
 my_path = '/'
 
+#new Global
+free_b,used_b,total_b = shutil.disk_usage(my_path)
+
 #function to get user account & time accessed
 def get_account():
     #get time accessed
@@ -20,19 +23,21 @@ def get_account():
     #print username logged in
     print(f"User: {user_acc}")
     #print time accessed - now
-    print(f"Last accessed: {time_acc}")
+    print(f"Last accessed: {time_acc} \n")
 
 #get capacity - in Bytes
-def get_capacity(my_path):
-    free_b,used_b,total_b = shutil.disk_usage(my_path)
+def get_capacity(my_path):    
     print(f"Capacity: {free_b} \n")
     #print('{:6.2f} GB'.format(TOTAL_AMOUNT))
     
-    
-  
+#print used space in Bytes    
+def get_usedSpace(my_path):
+      print(f"Used Space: {used_b} \n")
 
 
 #call get_account function
 get_account()   
 #get capacity 
 get_capacity(my_path)
+#get_usedSpace
+get_usedSpace(my_path)
